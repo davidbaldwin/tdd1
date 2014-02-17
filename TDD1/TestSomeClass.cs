6e323e43a514558b42e6fc52b7c6ebecf9735d91
davidbaldwin <david_o_baldwin@yahoo.com>
2014-02-17 12:20:52 +0000

    Created initial classes

---
+++ TDD1/TestSomeClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD1
+{
+    [TestClass]
+    public class TestSomeClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
