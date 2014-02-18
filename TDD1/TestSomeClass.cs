08ebc384159b75db7c232a3445d58cf619c2eea8
davidbaldwin <david_o_baldwin@yahoo.com>
2014-02-18 11:32:27 +0000

    Added failing test 0 returns 0

--- TDD1/TestSomeClass.cs
+++ TDD1/TestSomeClass.cs
@@ -7,8 +7,14 @@ namespace TDD1
     public class TestSomeClass
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestFib0Returns0()
         {
+            Assert.AreEqual(0, Fibonacci(0));
+        }
+
+        private object Fibonacci(object index)
+        {
+            return null;
         }
     }
 }
